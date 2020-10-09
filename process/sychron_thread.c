50e2c083b2d677314cc2ddddd6b343070eb82261
Myingyu <wu_mingyu@live.cn>
2020-10-09 23:52:24 +0800

    linux version

--- process/sychron_thread.c
+++ process/sychron_thread.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <semaphore.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 
 char buf[32];
 sem_t sem;
@@ -16,13 +18,14 @@ int main(int argc, char const *argv[])
 		perror("sem_init");
 		exit(-1);
 	}
-	if ( pthread_create(a_thread, NULL, function, NULL) != 0)
+	if ( pthread_create(&a_thread, NULL, function, NULL) != 0)
 	{
 		perror("pthread_create failed");
 		exit(-1);
 	}
 	do{
 		fgets(buf, 32, stdin);
+		sleep(5);
 		sem_post(&sem);
 	}while(strncmp(buf, "quit", 4) != 0);
 
@@ -31,7 +34,9 @@ int main(int argc, char const *argv[])
 }
 void *function(void *arg){
 	while(1){
+		sleep(5);
 		sem_wait(&sem);
+		sleep(5);
 		printf("your enter %lu characters\n", strlen(buf) );
 	}
 
