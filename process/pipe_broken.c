b91954417a6aff856ef1cf23b203d7dbfac8c5e2
Mingyu Wu <wu_mingyu@live.cn>
2020-10-10 16:06:25 +0800

    L5-D4-2

---
+++ process/pipe_broken.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char const *argv[])
+{
+	pid_t pid;
+	int pfd[2] , stauts;
+	char buf[32];
+	if ( pipe(pfd) < 0 )
+	{
+		perror("pipe failed!");exit(-1);
+	}
+	close (pfd[0]); //关闭读端
+	if ( pid == fork() < 0)
+	{
+		perror("fork failed"); exit(-1);
+	}else if ( pid == 0)
+	{ 	//子进程
+		write(pfd[1], buf, 32);
+		exit(0);
+	}else{
+		//父进程
+		wait(&stauts);
+		printf("%x\n", stauts);
+	}
+	return 0;
+}
