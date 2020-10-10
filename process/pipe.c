1f9810beafc5e4995ead4f6fc006fcad32a12350
Mingyu Wu <wu_mingyu@live.cn>
2020-10-10 14:31:00 +0800

    pipe

---
+++ process/pipe.c
@@ -0,0 +1,52 @@
+// 无名管道
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(void)
+{
+	pid_t pid1, pid2;
+	char buf[32];
+
+	int pfd[2];
+	if ( pipe(pfd) < 0 )
+	{
+		perror("pipe");
+		exit(-1);
+	}
+	if ( (pid1 = fork()) < 0 )
+	{
+		perror("fork");exit(-1);
+	}else if (  pid1 == 0 )
+	{
+		//子进程1
+		strcpy(buf,"Im process_1");
+		write(pfd[1], buf, 32);
+		exit(0);
+	}else{
+		//父进程
+		if ( (pid2 = fork() ) < 0 )
+		{
+			perror("fork");exit(-1);
+		}
+		else if ( pid2 == 0 ){
+			//子进程2
+			sleep(1);
+			strcpy(buf, "Im process_2");
+			write(pdf[1], buf, 32);
+		}else{
+			wait( NULL );
+			read(pfd[0], buf, 32);
+			printf("%s\n", buf);
+			wait( NULL );
+			read(pfd[0], buf, 32)
+			printf("%s\n", buf);
+		}
+
+	}
+	
+	return 0;
+}
