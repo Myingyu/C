07a4e3b8ff532f150af0324979ee7b2e8cf76891
Mingyu Wu <wu_mingyu@live.cn>
2020-10-10 09:16:15 +0800

    L5-D3-3

---
+++ process/sychron_thread_2.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+char buf[32];
+sem_t sem_w;
+sem_t sem_r;
+void *function(void *arg);
+
+int main(int argc, char const *argv[])
+{
+	pthread a_thread;
+
+
+	if ( sem_init(&sem_w,0, 1) < 0 )
+	{
+		perror("write: sem_init failed");
+		exit(-1);
+	}
+	if ( sem_init(&sem_r,0, 0) < 0 )
+	{
+		perror("write: sem_init failed");
+		exit(-1);
+	}
+	if ( ptread_create(&a_thread, NULL , function, NULL) != 0)
+	{
+		perror("pthread_create failed!");
+	}
+	do{
+		sem_post(sem_w);
+		sem_wait(sem_r);
+		fgets(buf, 32, stdin);
+		sleep(5);
+	}while(strncmp(buf, "quit", 4) != 0);
+
+	return 0;
+}
+
+void *function(void * arg){
+	while( 1 ){
+		sem_wait(&sem_w);
+		sem_post(&sem_r);
+		printf("your enter %lu characters\n", strlen(buf) );
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
