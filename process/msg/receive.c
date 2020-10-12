4cc028b8bc2d0bcd3bc70576c0ffcc0ac4f658f6
Mingyu Wu <wu_mingyu@live.cn>
2020-10-12 19:01:29 +0800

    L5-D6-4

---
+++ process/msg/receive.c
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+//消息的格式
+typedef struct {
+	long mtype;
+	char mtext[64];
+}MSG;
+
+#define LEN (sizeof(MSG) - sizeof(long)) //正文消息长度
+
+
+int main(int argc, char const *argv[])
+{
+	int msgid;
+	key_t key;
+
+	MSG buf;
+	buf.mtype = 100;
+
+	//生成key
+	if ( key=ftok(".", 'm') == -1 ){
+		perror("ftok");exit(-1);
+	}
+	//创建私有内存
+	if ( int shmid = shmget(key, 1024, IPC_CREAT|06666) < 0 )
+	{
+		perror("shmget error!");exit(-1);
+	}
+
+	if ( (msgid = msgget(key, IPC_CREAT|0666)) == -1)
+	{
+		perror("msgget failed");exit(-1);
+	}
+	if ( msgrcv(msgid, &buf, LEN, 0) < 0 )
+	{
+		perror("msgrcv failed");exit(-1);
+	}
+	
+	printf("received message: %s\n", buf);
+
+
+
+
+
+
+	return 0;
+}
---
+++ process/msg/send.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+//消息的格式
+typedef struct {
+	long mtype;
+	char mtext[64];
+}MSG;
+
+#define LEN (sizeof(MSG) - sizeof(long)) //正文消息长度
+
+
+int main(void)
+{
+	int msgid;
+	key_t key;
+
+	MSG buf;
+	buf.mtype = 100;
+	fgets(buf.mtext, 64, stdin);
+	//生成key
+	if ( key=ftok(".", 'm') == -1 ){
+		perror("ftok");exit(-1);
+	}
+	//创建私有内存
+	if ( int shmid = shmget(key, 1024, IPC_CREAT|06666) < 0 )
+	{
+		perror("shmget error!");exit(-1);
+	}
+	//消息队列创建
+	if( (msgid = msgget(key, IPC_CREAT|0666) == -1 )){
+		printf("msgget failed!\n");exit(-1);
+	}
+	if ( msgsnd(int msgid, &buf,LEN,0) ==-1 )
+	{
+		perror("msgsnd failed");exit(-1);
+	}
+
+	printf("send message: %s\n", buf);
+
+	return 0;
+}
