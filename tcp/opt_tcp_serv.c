37870663a56e99090c94c9a716a337a9fd3358cc
Mingyu Wu <wu_mingyu@live.cn>
2020-11-03 12:00:40 +0800

    L6-D3-4

--- tcp/opt_tcp_serv.c
+++ tcp/opt_tcp_serv.c
@@ -33,15 +33,17 @@ int main(int argc, char const *argv[])
 	// 2.1 填充 struct sockaddr_in 结构体变量
 	// sin 结构体清零
 	bzero(&sin, sizeof(sin));
-
+	// 优化1 ： 自动识别服务器IP，并且绑定主机IP
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(SERV_PORT); // 网络字节序的端口号
+
+	sin.sin_addr.s_addr = htol(INADDY_ANY);
 #if 0
 	sin.sin_addr.s_addr = inet_addr(SER_IP_ADDR);  // 将点分形式的IP地址转换成网络字节序
 #else
-	if(inet_pton(AF_INET, SERV_IP_ADDR, &sin.sin_addr.s_addr) < 1 ){
-		perror("socket failed!");
-	}
+	// if(inet_pton(AF_INET, SERV_IP_ADDR, &sin.sin_addr.s_addr) < 1 ){
+	// 	perror("socket failed!");
+	// }
 #endif	
 
 	// 2.2 绑定	
@@ -50,18 +52,32 @@ int main(int argc, char const *argv[])
 		  perror("bind");
 		  exit(-1);
 	}
-	// 优化1 ： 自动识别服务器IP，并且绑定主机IP
-	
 
 
 	// 3. 调用listen() 把 主动套接字变成被动套接字
 	listen(fd, BACKLOG);
 	// 4. 阻塞等待客户端请求连接
+
+	// 优化2.2 通过程序获取刚建立连接的socket的客户端的IP地址和端口号
 	int newfd = -1;
+#if 1
+	struct sockaddr_in cin;
+	if(newfd = accept(fd, (struct sockaddr*)&cin, sizeof(cin)) < 0){
+		perror("accept");
+		exit(-1);
+	}
+	char ipv4[16];
+	if (inet_ntop(AF_INET, (void*)&cin.sin_addr, sizeof(cin)) == 0){
+		perror("inet_ntop");
+	}
+	printf("Client from %s:%d connected!\n", ipv4, ntohs(cin.sin_port));
+
+#else
 	if ( (newfd = accept(fd, NULL, NULL)) < 0){
 		perror("accept");
 		exit(-1);
 	}
+#endif
 	// 5. 读写
 	// 与newfd进行数据的读写 
 	int ret = -1;
