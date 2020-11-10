008f3a6037956b4c15ddb565ff37f5df1ccf729b
Mingyu Wu <wu_mingyu@live.cn>
2020-11-10 23:59:31 +0800

    L6-D5-2

--- UDP/net.h
+++ UDP/net.h
@@ -9,7 +9,7 @@
 #define SERV_IP_ADDR "192.168.1.8"
 #define SERV_PORT 50002
 
-#define CLIENT_IP_ADDR "192.168.1.8"
+#define CLIENT_IP_ADDR "192.168.1.6"
 #define CLIENT_PORT 50010
 
 #define BUFSIZE 64
