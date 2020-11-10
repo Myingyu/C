72913fedb2987fa04babe1c72a4949f283baf458
Mingyu Wu <wu_mingyu@live.cn>
2020-11-10 23:49:35 +0800

    L6-D5-2

--- UDP/udp_serv.c
+++ UDP/udp_serv.c
@@ -36,9 +36,9 @@ int main(int argc, char const *argv[])
 	socklen_t cin_addrlen = sizeof(cin);
 	while(1){
 
-		recvfrom(sockfd, buf, BUFSIZE-1, MSG_DONTWAIT, (struct sockaddr*)&cin, sin_addrlen);
+		recvfrom(sockfd, buf, BUFSIZE-1, MSG_DONTWAIT, (struct sockaddr*)&cin, &sin_addrlen);
 		printf("receiving: %s\n", buf);
-		sendto(sockfd, buf, BUFSIZE-1, MSG_DONTWAIT, (struct sockaddr*)&sin, sin_addrlen);
+		sendto(sockfd, buf, BUFSIZE-1, MSG_DONTWAIT, (struct sockaddr*)&sin, &sin_addrlen);
 	}
 
 	return 0;
