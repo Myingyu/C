7426bc199e237d215cb2bd5cdaeb7f50701fa113
Mingyu Wu <wu_mingyu@live.cn>
2020-11-18 23:35:59 +0800

    L6-D6-4

--- IO/net.h
+++ IO/net.h
@@ -6,7 +6,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define SERV_IP_ADDR "192.30.255.113"
+#define SERV_IP_ADDR "81.70.145.175"
 #define SERV_PORT 50002
 #define BACKLOG 5
 // #define CLIENT_IP_ADDR "192.168.1.6"
