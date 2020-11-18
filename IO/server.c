14ce61bfdf1e713474870b3deb24dd9ec9dc15b4
Mingyu Wu <wu_mingyu@live.cn>
2020-11-19 00:38:10 +0800

    L6-D6-4

--- IO/server.c
+++ IO/server.c
@@ -38,27 +38,38 @@ int main(int argc, char const *argv[])
 	char serv_ipv4_addr[16];
 	char client_ipv4_addr[16];
 
+	if ( (accept_fd = accept(sockfd, (struct sockaddr*)&cin, &addr_len)) == -1){
+	perror("accpet");
+	exit(-1);
+}
+
 	inet_ntop(AF_INET, (void *)&cin.sin_addr.s_addr, serv_ipv4_addr, addr_len);
 	printf("Server IP: %s\n", serv_ipv4_addr);
 
 
+
 	while(1){
+		int ret = -1
 		bzero(buf, BUFSIZE-1);
 
-		if ( (accept_fd = accept(sockfd, (struct sockaddr*)&cin, &addr_len)) == -1){
-				perror("accpet");
+
+		do{
+			if((ret = read(newfd, buf, BUFSIZE-1)) == -1){
+				perror("read");
 				exit(-1);
 			}
+		}while(ret > 0 && EINTR == errno);
 
 		inet_ntop(AF_INET, (void *)&sin.sin_addr.s_addr, serv_ipv4_addr, addr_len);
+		inet_ntop(AF_INET, (void *)&cin.sin_addr.s_addr, client_ipv4_addr, sizeof(cin));
 		printf("Server IP:%s %d \n", serv_ipv4_addr, ntohs(sin.sin_port));
 		printf("Client IP:%s %d \n", client_ipv4_addr, ntohs(cin.sin_port));
-		read(accept_fd, buf, BUFSIZE-1);
+		// read(accept_fd, buf, BUFSIZE-1);
 		printf("%s", buf);
-		close(accept_fd);
+
 	}
 
-
+	close(accept_fd);
 	close(sockfd);
 	
 
