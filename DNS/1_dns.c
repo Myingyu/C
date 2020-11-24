4a09811323fd692997b750bf4ce61142fdde9ef7
Mingyu Wu <wu_mingyu@live.cn>
2020-11-24 16:55:12 +0800

    2020-11-24

--- DNS/1_dns.c
+++ DNS/1_dns.c
@@ -5,15 +5,15 @@ int main(int argc, char const *argv[])
 	struct hostent *hs = NULL;
 
 	char serv_ip[16];
-	inet_ntop(hs->h_addrtype, (void *)&hs->h_addr, serv_ip, hs->h_length);
-	printf("IP from gethostbyname: %s\n", serv_ip);
+	// inet_ntop(hs->h_addrtype, (void *)&hs->h_addr, serv_ip, hs->h_length);
+	// printf("IP from gethostbyname: %s\n", serv_ip);
 
 
 	if((hs = gethostbyname(SERV_DOMAIN)) == NULL){
 		herror("gethostbyname");
 		exit(-1);
 	}
-	printf("hs->h_addr: %s\n", hs->h_addr);
+	// printf("hs->h_addr: %s\n", hs->h_addr);
 
 	int sockfd = socket(AF_INET,  SOCK_STREAM, 0);
 
@@ -22,17 +22,23 @@ int main(int argc, char const *argv[])
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(SERV_PORT);
 	sin.sin_addr.s_addr = *(uint32_t *)hs->h_addr;
+	inet_ntop(AF_INET, (void *)&sin.sin_addr, serv_ip, sizeof(sin));
+
 
 	endhostent();
+
 	hs = NULL;
+
 
 	if ( connect(sockfd, (struct sockaddr *)&sin, sizeof(sin)) == -1){
 		perror("connect failed!");
 		exit(-1);
 	}
+	printf("connect to server %s:%d \n",serv_ip, SERV_PORT);
+	
 	char buf[BUFSIZE];
 	while(1){
-		bzero(buf,BUFSIZE);
+		bzero(buf,BUFSIZE-1);
 		fgets(buf, BUFSIZE-1, stdin);
 		write(sockfd, buf, BUFSIZE-1);
 
