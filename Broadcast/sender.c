45af370abd386a3f19f6caf97f0bfa4a0d6e5a8a
Mingyu Wu <wu_mingyu@live.cn>
2020-11-25 20:59:29 +0800

    L6-D9-1

--- Broadcast/sender.c
+++ Broadcast/sender.c
@@ -15,8 +15,8 @@ int main(int argc, char const *argv[])
 	bzero(&sin, sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(SERV_PORT);
-	sin.sin_addr.s_addr = SERV_IP_ADDR;
-	// inet_pton(AF_INET, SERV_IP_ADDR, &sin.sin_addr.s_addr);
+	// sin.sin_addr.s_addr = SERV_IP_ADDR;
+	inet_pton(AF_INET, SERV_IP_ADDR, &sin.sin_addr.s_addr);
 	int b_reuse = 1;
 	//允许发送广播数据包
 	setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &b_reuse, sizeof(b_reuse));
